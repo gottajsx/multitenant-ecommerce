8367d448535af9051bd6d40db349f4690b8ae51a
gottajsx <gottajsx@gmail.com>
2025-05-04 18:08:24 +0200

    Add Filter button

---
+++ src/app/(app)/(home)/search-filters/category-dropdown.tsx
@@ -0,0 +1,27 @@
+import { Button } from "@/components/ui/button";
+import { Category } from "@/payload-types";
+import { cn } from "@/lib/utils";
+
+interface Props {
+    category: Category;
+    isActive: boolean;
+    isNavigationHovered: boolean;
+};
+
+export const CategoryDropdown = ({
+    category,
+    isActive,
+    isNavigationHovered
+}: Props) => {
+    return (
+        <Button 
+            variant="elevated"
+            className={cn(
+                "h-11 px-4 bg-transparent border-transparent rounded-full hover:bg-white hover:border-primary text-black",
+                isActive && !isNavigationHovered && "bg-white border-primary"
+            )}
+        >
+            {category.name}
+        </Button>
+    );
+};

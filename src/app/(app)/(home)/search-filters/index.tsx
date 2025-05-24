7327ad376dba8cb005bb9630d97d916c2abf99b5
gottajsx <gottajsx@gmail.com>
2025-05-24 18:01:29 +0200

    06 Categories Finalization

--- src/app/(app)/(home)/search-filters/index.tsx
+++ src/app/(app)/(home)/search-filters/index.tsx
@@ -1,9 +1,9 @@
-import { Search } from "lucide-react";
+import { CustomCategory } from "../types";
 import { SearchInput } from "./search-input";
 import { Categories } from "./categories";
 
 interface Props {
-    data: any;
+    data: CustomCategory[];
 };
 
 export const SearchFilters = ({
@@ -11,8 +11,11 @@ export const SearchFilters = ({
 }: Props) => {
     return(
         <div className="px-4 lg:px-12 py-8 border-b flex flex-col gap-4 w-full">
-            <SearchInput />
-            <Categories data={data} />
+            <SearchInput data={data} />
+            <div className="hidden lg:block">
+                <Categories data={data} />
+            </div>
+            
         </div>
     );
 };

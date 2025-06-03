2c5737a7aee2ff71ac6394082a4e2198a41d5774
gottajsx <gottajsx@gmail.com>
2025-06-03 20:28:04 +0200

    <start signup frontend

--- src/app/(app)/(home)/navbar.tsx
+++ src/app/(app)/(home)/navbar.tsx
@@ -8,6 +8,7 @@ import { MenuIcon } from "lucide-react";
 
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
+
 import { NavbarSidebar } from "./navbar-sidebar";
 
 
@@ -90,7 +91,7 @@ export const Navbar = () => {
                     variant="secondary"
                     className="border-l border-t-0 border-b-0 border-r-2 px-12 h-full rounded-none bg-white hover:bg-pink-400 transition-colors text-lg"
                 >
-                    <Link href="/sign-in">
+                    <Link prefetch href="/sign-in">
                         Log in
                     </Link>
                 </Button>
@@ -98,7 +99,7 @@ export const Navbar = () => {
                     asChild
                     className="border-l border-t-0 border-b-0 border-r-2 px-12 h-full rounded-none bg-black text-white hover:bg-pink-400 hover:text-black"
                 >
-                    <Link href="/sign-up">
+                    <Link prefetch href="/sign-up">
                         Start selling
                     </Link>
                     

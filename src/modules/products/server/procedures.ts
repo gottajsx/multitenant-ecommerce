251f416ec00177e1d4db4c56994b599e8f36ead9
gottajsx <gottajsx@gmail.com>
2025-06-11 16:04:41 +0200

    In progress

--- src/modules/products/server/procedures.ts
+++ src/modules/products/server/procedures.ts
@@ -1,14 +1,47 @@
+import z from "zod";
+
 
 import { baseProcedure, createTRPCRouter } from "@/trpc/init";
+import { Where } from "payload";
 
 
 export const productsRouter = createTRPCRouter({
-    getMany: baseProcedure.query(async({ ctx }) => {
-        const data = await ctx.db.find({
-            collection: "products",
-            depth: 1, // Populate "category", "image"
-        });
+    getMany: baseProcedure
+        .input(
+            z.object({
+                category: z.string().nullable().optional(),
+            })
+        )
+        .query(async({ ctx, input }) => {
+            const where: Where = {};
 
-        return data;
-    }),
+            if (input.category) {
+                const categoriesData = await ctx.db.find({
+                    collection: "categories",
+                    limit: 1,
+                    pagination: false,
+                    where: {
+                        slug: {
+                            equals: input.category
+                        }
+                    }
+                });
+
+                const parentCategory = categoriesData.docs[0];
+            
+                if (parentCategory) {
+                    where["category.slug"] = {
+                        equals: parentCategory.slug,
+                    }
+                }
+            }
+
+            const data = await ctx.db.find({
+                collection: "products",
+                depth: 1, // Populate "category", "image"
+                where
+            });
+
+            return data;
+        }),
 });

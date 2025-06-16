e30ab3bab344ef9c57f8dccf4e459089a71bb4ae
gottajsx <gottajsx@gmail.com>
2025-06-16 11:42:46 +0200

    in progress

--- src/app/(app)/(home)/[category]/page.tsx
+++ src/app/(app)/(home)/[category]/page.tsx
@@ -1,3 +1,4 @@
+import { ProductFilters } from "@/modules/products/ui/components/product-filters";
 import { ProductList, ProductListSkeleton } from "@/modules/products/ui/components/product-list";
 import { getQueryClient, trpc } from "@/trpc/server";
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
@@ -22,9 +23,7 @@ const Page = async ({ params }: Props) => {
             <div className="px-4 lg:px-12 py-8 flex flex-col gap-4">
                 <div className="grid grid-cols-1 lg:grid-cols-6 xl:grid-cols-8 gap-y-6 gap-x-12">
                     <div className="lg:col-span-2 xl:col-span-2">
-                        <div className="border p-2">
-                            Product Filters
-                        </div>
+                        <ProductFilters />
                     </div>
                     <div className="lg:col-span-4 xl:col-span-6">
                         <Suspense fallback={<ProductListSkeleton />}>

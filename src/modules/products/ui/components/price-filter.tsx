b5954ca5d354a343212bd1ece128ec123ad1931f
gottajsx <gottajsx@gmail.com>
2025-06-16 12:55:49 +0200

    in progress

--- src/modules/products/ui/components/price-filter.tsx
+++ src/modules/products/ui/components/price-filter.tsx
@@ -1,4 +1,7 @@
-import { forbidden } from "next/navigation";
+import { ChangeEvent } from "react";
+
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
 interface Props {
     minPrice?: string | null;
@@ -7,7 +10,7 @@ interface Props {
     onMaxPriceChange: (value: string) => void;
 }
 
-export const formatAsCurrent = (value: string) => {
+export const formatAsCurrency = (value: string) => {
     const numéricValue = value.replace(/[^0-9.]/g, "");
 
     const parts = numéricValue.split(".");
@@ -25,3 +28,49 @@ export const formatAsCurrent = (value: string) => {
         maximumFractionDigits: 2,
     }).format(numberValue);
 };
+
+export const PriceFilter = ({
+    minPrice,
+    maxPrice,
+    onMinPriceChange,
+    onMaxPriceChange,
+}: Props) => {
+    const handleMinPriceChange = (e: ChangeEvent<HTMLInputElement>) => {
+        // Get the raw input value and extract only numeric values
+        const numericValue = e.target.value.replace(/[^0-9.]/g, "");
+        onMinPriceChange(numericValue);
+    };
+
+    const handleMaxPriceChange = (e: ChangeEvent<HTMLInputElement>) => {
+        // Get the raw input value and extract only numeric values
+        const numericValue = e.target.value.replace(/[^0-9.]/g, "");
+        onMaxPriceChange(numericValue);
+    };
+
+    return (
+        <div className="flex flex-col gap-2">
+            <div className="flex flex-col gap-2">
+                <Label className="font-medium text-base">
+                    Minimum price
+                </Label>
+                <Input 
+                    type="text"
+                    placeholder="$0"
+                    value={minPrice ? formatAsCurrency(minPrice) : ""}
+                    onChange={handleMinPriceChange}
+                />
+            </div>
+            <div className="flex flex-col gap-2">
+                <Label className="font-medium text-base">
+                    Maximum price
+                </Label>
+                <Input 
+                    type="text"
+                    placeholder="∞" // Infinity sign
+                    value={maxPrice ? formatAsCurrency(maxPrice) : ""}
+                    onChange={handleMaxPriceChange}
+                />
+            </div>
+        </div>
+    )
+}
--- src/modules/products/ui/components/product-filters.tsx
+++ src/modules/products/ui/components/product-filters.tsx
@@ -3,6 +3,9 @@
 import { cn } from "@/lib/utils";
 import { ChevronDownIcon, ChevronRightIcon } from "lucide-react";
 import { useState } from "react";
+import { useProductFilters } from "../../hooks/use-product-filters";
+import { unknown } from "zod";
+import { PriceFilter } from "./price-filter";
 
 interface ProductFiltersProps {
     title: string;
@@ -33,6 +36,12 @@ const ProductFilter = ({title, className, children}: ProductFiltersProps) => {
 };
 
 export const ProductFilters = () => {
+    const [filters, setFilters] = useProductFilters();
+
+    const onChange = (key: keyof typeof filters, value: unknown) => {
+        setFilters({ ...filters, [key]: value });
+    };
+
     return(
         <div className="border rounded-md bg-white">
             <div className="⁻4 border-b flex items-center justify-between">
@@ -42,7 +51,13 @@ export const ProductFilters = () => {
                 </button>
             </div>
             <ProductFilter title="Price" className="border-b-0">
-                <p>Price filter!</p>
+                <PriceFilter 
+                    minPrice={filters.minPrice}
+                    maxPrice={filters.maxPrice}
+                    onMinPriceChange={(value) => onChange("minPrice", value)}
+                    onMaxPriceChange={(value) => onChange("maxPrice", value)}
+                />
+
             </ProductFilter>
         </div>     
     )

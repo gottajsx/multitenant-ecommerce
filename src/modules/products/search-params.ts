8b9249df332d4cdf193205530b45c7a413d1cf20
gottajsx <gottajsx@gmail.com>
2025-06-17 14:26:13 +0200

    In progress

--- src/modules/products/search-params.ts
+++ src/modules/products/search-params.ts
@@ -1,18 +1,24 @@
-import { createLoader, parseAsString, parseAsArrayOf} from "nuqs/server";
+import { createLoader, parseAsString, parseAsArrayOf, parseAsStringLiteral} from "nuqs/server";
+
+const sortValues = ["curated", "trending", "hot_and_new"] as const;
 
 export const params = {
+    sort: parseAsStringLiteral(sortValues).withDefault("curated"),
     minPrice: parseAsString
         .withOptions({
              clearOnDefault: true,
-        }),
+        })
+        .withDefault(""),
     maxPrice: parseAsString
         .withOptions({
             clearOnDefault: true,
-        }),
+        })
+        .withDefault(""),
     tags: parseAsArrayOf(parseAsString)
         .withOptions({
             clearOnDefault: true,
         })
+        .withDefault([]),
 };
 
 export const loadProductFilters = createLoader(params);

77711ed2ad6a117062f245ac9ca2f9cfeaa486ec
gottajsx <gottajsx@gmail.com>
2025-06-17 14:51:30 +0200

    13-API Filters Sorting

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,11 +1,13 @@
-import { authRouter } from '@/modules/auth/server/procedures';
 import { createTRPCRouter } from '../init';
 
-import { categoriesRouter } from '@/modules/categories/server/procedures';
+import { authRouter } from '@/modules/auth/server/procedures';
+import { tagsRouter } from '@/modules/tags/server/procedures';
 import { productsRouter } from '@/modules/products/server/procedures';
+import { categoriesRouter } from '@/modules/categories/server/procedures';
 
 export const appRouter = createTRPCRouter({
   auth: authRouter,
+  tags: tagsRouter,
   products: productsRouter,
   categories: categoriesRouter,
 });

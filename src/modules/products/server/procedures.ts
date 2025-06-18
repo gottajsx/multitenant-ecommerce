b7a73ded1656dc4ff31223c5a40f8684a86b3bd0
gottajsx <gottajsx@gmail.com>
2025-06-18 10:47:00 +0200

    in progress

--- src/modules/products/server/procedures.ts
+++ src/modules/products/server/procedures.ts
@@ -4,11 +4,14 @@ import { Sort, Where } from "payload";
 import { Category, Media } from "@/payload-types";
 import { baseProcedure, createTRPCRouter } from "@/trpc/init";
 import { sortValues } from "../search-params";
+import { DEFAULT_LIMIT } from "@/constants";
 
 export const productsRouter = createTRPCRouter({
     getMany: baseProcedure
         .input(
             z.object({
+                cursor: z.number().default(1),
+                limit: z.number().default(DEFAULT_LIMIT),
                 category: z.string().nullable().optional(),
                 minPrice: z.string().nullable().optional(),
                 maxPrice: z.string().nullable().optional(),
@@ -94,6 +97,8 @@ export const productsRouter = createTRPCRouter({
                 depth: 1, // Populate "category", "image"
                 where,
                 sort,
+                page: input.cursor,
+                limit: input.limit,
             });
 
             return {

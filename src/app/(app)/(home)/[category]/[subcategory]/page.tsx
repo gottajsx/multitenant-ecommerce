4da08186e0a2c342f35ce82703b362c589c5ba45
gottajsx <gottajsx@gmail.com>
2025-06-18 11:37:32 +0200

    Seconf bug fix

--- src/app/(app)/(home)/[category]/[subcategory]/page.tsx
+++ src/app/(app)/(home)/[category]/[subcategory]/page.tsx
@@ -5,6 +5,8 @@ import { getQueryClient, trpc } from "@/trpc/server";
 
 import { ProductListView } from "@/modules/products/ui/views/product-list-view";
 import { loadProductFilters } from "@/modules/products/search-params";
+import { DEFAULT_LIMIT } from "@/constants";
+
 
 interface Props {
     params: Promise<{
@@ -20,9 +22,10 @@ const Page = async ({ params, searchParams}: Props) => {
     console.log(JSON.stringify(filters), "THIS IS FROM RSC");
 
     const queryClient = getQueryClient();
-    void queryClient.prefetchQuery(trpc.products.getMany.queryOptions({
-        category: subcategory,
+    void queryClient.prefetchInfiniteQuery(trpc.products.getMany.infiniteQueryOptions({
         ...filters,
+        category: subcategory,
+        limit: DEFAULT_LIMIT,
     }));
 
     return(
--- src/app/(app)/(home)/[category]/page.tsx
+++ src/app/(app)/(home)/[category]/page.tsx
@@ -6,6 +6,7 @@ import { getQueryClient, trpc } from "@/trpc/server";
 
 import { loadProductFilters } from "@/modules/products/search-params";
 import { ProductListView } from "@/modules/products/ui/views/product-list-view";
+import { DEFAULT_LIMIT } from "@/constants";
 
 
 interface Props {
@@ -22,9 +23,10 @@ const Page = async ({ params, searchParams}: Props) => {
     console.log(JSON.stringify(filters), "THIS IS FROM RSC");
 
     const queryClient = getQueryClient();
-    void queryClient.prefetchQuery(trpc.products.getMany.queryOptions({
-        category,
+    void queryClient.prefetchInfiniteQuery(trpc.products.getMany.infiniteQueryOptions({
         ...filters,
+        category,
+        limit: DEFAULT_LIMIT,
     }));
 
     return(

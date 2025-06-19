1688327282a12c7b545d03d6b3a58fa4352d16e9
gottajsx <gottajsx@gmail.com>
2025-06-19 10:42:09 +0200

    In progress

--- src/modules/products/server/procedures.ts
+++ src/modules/products/server/procedures.ts
@@ -17,6 +17,7 @@ export const productsRouter = createTRPCRouter({
                 maxPrice: z.string().nullable().optional(),
                 tags: z.array(z.string()).nullable().optional(),
                 sort: z.enum(sortValues).nullable().optional(),
+                tenantSlug: z.string().nullable().optional(),
             })
         )
         .query(async({ ctx, input }) => {
@@ -48,6 +49,12 @@ export const productsRouter = createTRPCRouter({
                 where.price = {
                     less_than_equal: input.maxPrice,
                 }
+            }
+
+            if (input.tenantSlug) {
+                where["tenant.slug"] = {
+                    equals: input.tenantSlug,
+                };
             }
 
             if (input.category) {

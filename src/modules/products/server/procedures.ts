8d0e9f1f0d3bd6fcc64f9b29ce79a668b40cc666
gottajsx <gottajsx@gmail.com>
2025-06-19 16:54:35 +0200

    16-Tenant-Pages

--- src/modules/products/server/procedures.ts
+++ src/modules/products/server/procedures.ts
@@ -1,7 +1,7 @@
 import z from "zod";
 import { Sort, Where } from "payload";
 
-import { Category, Media } from "@/payload-types";
+import { Category, Media, Tenant } from "@/payload-types";
 import { baseProcedure, createTRPCRouter } from "@/trpc/init";
 import { sortValues } from "../search-params";
 import { DEFAULT_LIMIT } from "@/constants";
@@ -17,6 +17,7 @@ export const productsRouter = createTRPCRouter({
                 maxPrice: z.string().nullable().optional(),
                 tags: z.array(z.string()).nullable().optional(),
                 sort: z.enum(sortValues).nullable().optional(),
+                tenantSlug: z.string().nullable().optional(),
             })
         )
         .query(async({ ctx, input }) => {
@@ -48,6 +49,12 @@ export const productsRouter = createTRPCRouter({
                 where.price = {
                     less_than_equal: input.maxPrice,
                 }
+            }
+
+            if (input.tenantSlug) {
+                where["tenant.slug"] = {
+                    equals: input.tenantSlug,
+                };
             }
 
             if (input.category) {
@@ -94,7 +101,7 @@ export const productsRouter = createTRPCRouter({
 
             const data = await ctx.db.find({
                 collection: "products",
-                depth: 1, // Populate "category", "image"
+                depth: 2, // Populate "category", "image", "tenant" & "tenant.image"
                 where,
                 sort,
                 page: input.cursor,
@@ -106,6 +113,7 @@ export const productsRouter = createTRPCRouter({
                 docs: data.docs.map((doc) => ({
                     ...doc,
                     image: doc.image as Media | null,
+                    tenant: doc.tenant as Tenant & { image: Media | null },
                 }))
             };
         }),

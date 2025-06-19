7ab2db278b581a451f73e17b0d3caaa0af68a143
gottajsx <gottajsx@gmail.com>
2025-06-19 16:53:09 +0200

    In progress

--- src/app/(app)/{tenants}/tenants/[slug]/(home)/page.tsx
+++ src/app/(app)/{tenants}/tenants/[slug]/(home)/page.tsx
@@ -27,7 +27,7 @@ const Page = async({ params, searchParams}: Props) => {
 
     return(
         <HydrationBoundary state={dehydrate(queryClient)}>
-           <ProductListView tenantSlug={slug}/>
+           <ProductListView tenantSlug={slug} narrowView/>
         </HydrationBoundary>
     );
 }
--- src/modules/products/ui/components/product-list.tsx
+++ src/modules/products/ui/components/product-list.tsx
@@ -9,14 +9,16 @@ import { Button } from "@/components/ui/button";
 
 import { useProductFilters } from "../../hooks/use-product-filters";
 import { ProductCard, ProductCardSkeleton } from "./product-card";
+import { cn } from "@/lib/utils";
 
 
 interface Props {
     category?: string;
     tenantSlug?: string;
+    narrowView?: boolean;
 }
 
-export const ProductList = ({ category, tenantSlug }: Props) => {
+export const ProductList = ({ category, tenantSlug, narrowView }: Props) => {
     const [filters] = useProductFilters();
 
     const trpc = useTRPC();
@@ -45,7 +47,10 @@ export const ProductList = ({ category, tenantSlug }: Props) => {
 
     return(
         <>
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-4">
+            <div className={cn(
+                "grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-4",
+                narrowView && "lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-3"
+            )}>
                 {data?.pages.flatMap((page) => page.docs).map((product) => (
                     <ProductCard 
                         key={product.id}
@@ -77,9 +82,12 @@ export const ProductList = ({ category, tenantSlug }: Props) => {
     );
 };
 
-export const ProductListSkeleton = () => {
+export const ProductListSkeleton = ({ narrowView}: Props) => {
     return(
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-4">
+        <div className={cn(
+            "grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-4",
+            narrowView && "lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-3"
+        )}>
             {Array.from({ length: DEFAULT_LIMIT }).map((_, index) => (
                 <ProductCardSkeleton key={index} />
             ))}

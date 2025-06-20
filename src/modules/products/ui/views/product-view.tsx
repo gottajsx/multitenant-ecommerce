96d3547d22a4cdaa140e69b83a3cb73fd222f923
gottajsx <gottajsx@gmail.com>
2025-06-20 14:53:09 +0200

    in progress

--- src/modules/products/ui/views/product-view.tsx
+++ src/modules/products/ui/views/product-view.tsx
@@ -1,11 +1,15 @@
 "use client";
 
 import { StarRating } from "@/components/star-rating";
+import { Button } from "@/components/ui/button";
+import { Progress } from "@/components/ui/progress";
 import { formatCurrency, generateTenantURL } from "@/lib/utils";
 import { useTRPC } from "@/trpc/client";
 import { useSuspenseQuery } from "@tanstack/react-query";
+import { LinkIcon, StarIcon } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
+import { Fragment } from "react";
 
 interface ProductViewProps {
     productId: string;
@@ -64,9 +68,93 @@ export const ProductView = ({ productId, tenantSlug}: ProductViewProps) => {
                                 </div>
                             </div>
                         </div>
+
+                        <div className="block lg:hidden px-6 py-4 items-center justify-center border-b">
+                            <div className="flex items-center gap-1">
+                                <StarRating 
+                                    rating={3}
+                                    iconClassName="size-4"
+                                />
+                                <p className="text-base font-medium">
+                                    {5} ratings
+                                </p>
+                            </div>
+                        </div>  
+
+                        <div className="p-6">
+                            {data.description ? (
+                                <p>{data.description}</p>
+                            ) : (
+                                <p className="font-medium text-muted-foreground italic">
+                                    No description provided
+                                </p>
+                            )}
+
+                        </div>
+
                     </div>
+
+                    <div className="col-span-2">
+                        <div className="border-t lg:border-t-0 lg:border-l h-full">
+                            <div className="flex flex-col gap-4 p-6 border-b">
+                                <div className="flex flex-row items-center gap-2">
+                                    <Button
+                                        variant="elevated"
+                                        className="flex-1 bg-pink-400"
+                                    >
+                                        Add to cart
+                                    </Button>
+                                    <Button
+                                        className="size-12"
+                                        variant="elevated"
+                                        onClick={() => {}}
+                                        disabled={false}
+                                    >
+                                        <LinkIcon />
+                                    </Button>
+                                </div>
+
+                                <p className="text-center font-medium">
+                                    {data.refundPolicy === "no-refunds"
+                                        ? "No refunds"
+                                        : `${data.refundPolicy} money back guarentee`
+                                    }
+                                </p>
+                            </div>
+
+                            <div className="p-6">
+                                <div className="flex items-center justify-between">
+                                    <h3 className="text-xl font-medium">Ratings</h3>
+                                    <div className="flex items-center gap-x-1 font-medium">
+                                        <StarIcon className="size-4 fill-black" />
+                                        <p>({5})</p>
+                                        <p className="text-base">{5} ratings</p>
+                                    </div>
+                                </div>
+
+                                <div
+                                    className="grid grid-cols-[auto_1fr_auto] gap-3 mt-4"
+                                >
+                                    {[5, 4, 3, 2, 1].map((stars) => (
+                                        <Fragment key={stars}>
+                                            <div className="font-medium">{stars} {stars === 1 ? "star" : "stars"}</div>
+                                            <Progress 
+                                                value={0}
+                                                className="h-[1lh]"
+                                            />
+                                            <div className="font-medium">
+                                                {0}%
+                                            </div>
+                                        </Fragment>
+                                    ))}
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};

d4431a432e06de540209494f9f2d5835587c590f
gottajsx <gottajsx@gmail.com>
2025-06-20 10:59:27 +0200

    in progress

--- src/modules/products/server/procedures.ts
+++ src/modules/products/server/procedures.ts
@@ -7,6 +7,20 @@ import { sortValues } from "../search-params";
 import { DEFAULT_LIMIT } from "@/constants";
 
 export const productsRouter = createTRPCRouter({
+    getOne: baseProcedure
+        .input(
+            z.object({
+                id: z.string(),
+            })
+        )
+        .query(async({ ctx, input }) => {
+            const product = await ctx.db.findByID({
+                collection: "products",
+                id: input.id,
+            });
+
+            return product;
+        }),
     getMany: baseProcedure
         .input(
             z.object({
---
+++ src/modules/products/ui/views/product-view.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+
+interface ProductViewProps {
+    productId: string;
+    tenantSlug: string;
+}
+
+export const ProductView = ({ productId, tenantSlug}: ProductViewProps) => {
+    return(
+        <div className="px-4 lg:px-12 py-10">
+            <div className="border rounded-sm bg-white overflow-hidden">
+                <div className="relative aspect-[3.9] border-b">
+                   <Image 
+                        src="/placeholder.png"
+                        alt="cover"
+                        fill
+                        className="object-cover"
+                   /> 
+                </div>
+            </div>
+        </div>
+    )
+}

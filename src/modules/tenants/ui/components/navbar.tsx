d485ba106acf1560d5a873236661aa6e1abcca16
gottajsx <gottajsx@gmail.com>
2025-06-23 12:08:19 +0200

    Work

--- src/modules/tenants/ui/components/navbar.tsx
+++ src/modules/tenants/ui/components/navbar.tsx
@@ -1,11 +1,29 @@
 "use client";
 
-import Link from "next/link";
-import { useTRPC } from "@/trpc/client";
 import { useSuspenseQuery } from "@tanstack/react-query";
 import Image from "next/image";
-import { generateTenantURL } from "@/lib/utils";
+import dynamic from "next/dynamic";
+import { ShoppingCartIcon } from "lucide-react";
+import Link from "next/link";
 
+import { useTRPC } from "@/trpc/client";
+import { generateTenantURL } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+//import { CheckoutButton } from "@/modules/checkout/ui/components/checkout-button";
+
+
+const CheckoutButton = dynamic(
+    () => import("@/modules/checkout/ui/components/checkout-button").then(
+        (mod) => mod.CheckoutButton,
+    ),
+    {
+        ssr: false,
+        loading: () => (
+            <Button disabled className="bg-white">
+                <ShoppingCartIcon className="text-black"/>
+            </Button>)
+    },
+);
 
 interface Props {
     slug: string;
@@ -30,6 +48,7 @@ export const Navbar = ({ slug }: Props) => {
                     )}
                     <p className="text-xl">{data.name}</p>
                 </Link>
+                <CheckoutButton tenantSlug={slug} />
             </div>
         </nav>
     );

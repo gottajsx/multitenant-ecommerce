e2c903d75b48a05edf4a96df55a1257c89157ef4
gottajsx <gottajsx@gmail.com>
2025-06-26 11:32:14 +0200

    work

---
+++ src/modules/library/ui/views/product-view.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import Link from "next/link";
+import { ArrowLeftIcon } from "lucide-react";
+
+import { useSuspenseQuery } from "@tanstack/react-query";
+import { useTRPC } from "@/trpc/client";
+
+
+interface Props {
+    productId: string;
+}
+
+
+export const ProductView = ({ productId }: Props) => {
+    const trpc = useTRPC();
+    const { data } = useSuspenseQuery(trpc.library.getOne.queryOptions({
+        productId,
+    }));
+    
+    return (
+        <div className="min-h-screen bg-white">
+            <nav className="p-4 bg-[#F4F4F0] w-full border-b">
+                <Link prefetch href="/library" className="flex items-center gap-2">
+                    <ArrowLeftIcon className="size-4" />
+                    <span className="text font-medium">Back to library</span>
+                </Link>
+                
+            </nav>
+            <header className="bg-[#F4F4F0] py-8 border-b">
+                <div className="max-w-(--breakpoint-xl) mx-auto px-4 lg:px-12">
+                    <h1 className="text-[40px] font-medium">{data.name}</h1>
+                </div>
+            </header>
+            <section className="max-w-(--breakpoint-xl) mx-auto px-4 lg:px-12 py-10">
+               
+            </section>
+        </div>
+    );
+};

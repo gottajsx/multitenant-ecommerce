e616f40a9c081da7ce73810d90b4dbabd14c4004
gottajsx <gottajsx@gmail.com>
2025-06-26 10:58:07 +0200

    create getone library procedure

--- src/modules/library/server/procedures.ts
+++ src/modules/library/server/procedures.ts
@@ -1,5 +1,5 @@
 import z from "zod";
-import { Sort, Where } from "payload";
+import { TRPCError } from "@trpc/server";
 
 import { DEFAULT_LIMIT } from "@/constants";
 import { Media, Tenant } from "@/payload-types";
@@ -7,7 +7,58 @@ import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
 
 
 
+
 export const libraryRouter = createTRPCRouter({
+    getOne: protectedProcedure
+        .input(
+            z.object({
+                productId: z.string(),
+            }),
+        )
+        .query(async({ ctx, input }) => {
+            const ordersData = await ctx.db.find({
+                collection: "orders",
+                limit: 1,
+                pagination: false,
+                where: {
+                    and: [
+                        {
+                            product: {
+                                equals: input.productId,
+                            }
+                        },
+                        {
+                            user: {
+                                equals: ctx.session.user.id,
+                            },
+                        },
+                    ],
+                },
+            });
+
+            const order = ordersData.docs[0];
+
+            if(!order) {
+                throw new TRPCError({
+                    code: "NOT_FOUND",
+                    message: "Order not found",
+                })
+            }
+
+            const product = await ctx.db.findByID({
+                collection: "products",
+                id: input.productId,
+            });
+
+            if (!product) {
+                throw new TRPCError({
+                    code: "NOT_FOUND",
+                    message: "Product not found",
+                });
+            };
+
+            return product;
+        }),
     getMany: protectedProcedure
         .input(
             z.object({

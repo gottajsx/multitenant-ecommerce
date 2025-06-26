2f63d21f260532cb36dfe0270a2a423cf2e049e0
gottajsx <gottajsx@gmail.com>
2025-06-26 11:50:28 +0200

    work

--- src/modules/library/ui/views/product-view.tsx
+++ src/modules/library/ui/views/product-view.tsx
@@ -33,7 +33,22 @@ export const ProductView = ({ productId }: Props) => {
                 </div>
             </header>
             <section className="max-w-(--breakpoint-xl) mx-auto px-4 lg:px-12 py-10">
-               
+               <div className="grid grid-cols-1 lg:grid-cols-7 gap-4 lg:gap-16">
+
+                <div className="lg:col-span-2">
+                    <div className="p-4 bg-white rounded-md border gap-4">
+                        TODO: Review Sidebar
+                    </div>
+                </div>
+
+                <div className="lg:col-span-5">
+                    <p className="font-medium italic text-muted-foreground">
+                        No special content
+                    </p>
+                </div>
+
+
+               </div>
             </section>
         </div>
     );
